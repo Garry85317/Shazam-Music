5c3c72621c315220170824ababc6664d1a760235
garry85317@gmail.com <garry85317@gmail.com>
2022-12-12 01:42:19 +0800

    Update tailwind.config.cjs
    styled

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: '#191624',
+        black: '#2e2e2e',
       },
       animation: {
         slideup: 'slideup 1s ease-in-out',
